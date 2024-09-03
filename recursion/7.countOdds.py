2e4f5421c8129d2d871ad34d56e38cb2c0fa1765
phanendra guptha katta <phanendraguptha@phanendras-MacBook-Air.local>
2024-09-02 21:23:04 -0500

    updated countOdds

--- recursion/7.countOdds.py
+++ recursion/7.countOdds.py
@@ -13,4 +13,28 @@ def helper(arr, index, res):
     res.append(arr[index])
   return helper(arr, index+1, res)
 
-countOdds([1, 7, 10, 3, 5, 9])
+# countOdds([1, 7, 10, 3, 5, 9])
+
+def countOdds2(arr):
+  data = helper(arr, 0)
+  print(data)
+
+
+def helper(arr, index):
+  res = []
+
+  # base condition
+  if index == len(arr):
+    return res
+  
+  # add the number to the res if it is odd
+  if arr[index] % 2 != 0:
+    res.append(arr[index])
+  
+  # previous call answers are appended to the current list
+  answerFromPrevCalls = helper(arr, index + 1)
+  res.extend(answerFromPrevCalls)
+
+  return res
+
+countOdds2([1, 7, 10, 3, 5, 9])
